a222afb9d4012d79d21fe76c14341c8d1a040ec2
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-10-30 15:54:52 -0300

    feat: use button as component

--- src/components/TopSection.tsx
+++ src/components/TopSection.tsx
@@ -1,33 +1,25 @@
+import { Button } from "./Button";
 import { ResponsibleContainer } from "./ResponsibleContainer";
 
 export function TopSection(props: { username: string }) {
   return (
-    <div className="bg-primary">
+    <div className="bg-primary md:px-8">
       <ResponsibleContainer
         style="
+          justify-between items-center 
           text-white py-6 flex-col text-center
           md:flex-row md:text-left md:items-start md:py-10
         "
       >
         <article className="">
-          <p className="text-2xl md:text-3xl lg:text-4xl">Olá {props.username}!</p>
+          <p className="text-3xl md:text-4xl ">Olá {props.username}!</p>
           <p className="opacity-75 mt-2 mb-7 lg:mb-0">Visualize e gerencie suas tarefas por aqui</p>
         </article>
 
-        <button className="
-          text-primary
-          bg-accent
-          px-5 py-2
-          rounded-sm
-          w-[200px]
-          hover:bg-dark
-          flex justify-between items-center
-          mx-auto
-          md:m-0
-        ">
+        <Button>
           Nova Tarefa
           <i className="fa-solid fa-plus border-2 border-primary rounded-full p-1 ml-4"></i>
-        </button>
+        </Button>
       </ResponsibleContainer>
     </div>
   );
