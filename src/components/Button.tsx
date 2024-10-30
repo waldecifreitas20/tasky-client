16e0713d3f87664c96eac681806a100d39cdca05
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-10-30 16:11:52 -0300

    fix: task card button width

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,20 +1,24 @@
 import { PropsWithChildren } from "react";
 
-export function Button(props: PropsWithChildren<{ border?: string, backgroundColor?: string }>) {
+interface ButtonProps {
+  style?: string,
+  border?: string,
+  backgroundColor?: string
+}
+
+export function Button(props: PropsWithChildren<ButtonProps>) {
   return (
     <>
       <button className={`
         text-primary
         px-5 py-2
         rounded-sm
-        w-[200px]
         hover:bg-dark
-        flex justify-between items-center
-        mx-auto
         md:m-0
         
         ${props.backgroundColor ?? "bg-accent"}
         ${props.border ?? ""}
+        ${props.style ?? ""}
       `}>
         {props.children}
       </button>
