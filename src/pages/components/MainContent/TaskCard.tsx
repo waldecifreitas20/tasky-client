fa6353b52f1b5213b7d97d827ef276025cd64cbc
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-10-31 15:29:30 -0300

    style: task card sizing

--- src/pages/components/MainContent/TaskCard.tsx
+++ src/pages/components/MainContent/TaskCard.tsx
@@ -14,22 +14,28 @@ export function TaskCard(props: { task: Task }) {
         rounded-md 
         px-4 py-6 
         h-[400px]
-        sm:max-w-[400px]
+        max-w-[380px]  
         md:max-w-[350px]
+        lg:max-w-[300px]
       ">
         <div>
           <div className="text-zinc-700 text-sm  flex justify-between">
             <p>{task.date}</p>
             <p>{task.hour ?? "24hrs"}</p>
           </div>
-          <p className="text-2xl font-bold mt-3 mb-2">{task.name}</p>
-
-          <p className="text-neutral-500">{task.desc}</p>
+          <p className="text-xl font-medium mt-3 mb-2">{task.name}</p>
+          <p className="text-sm text-neutral-500">{task.desc}</p>
         </div>
 
         <div>
-          <Button style="py-3 w-full mb-2">Editar</Button>
-          <Button style="py-3 w-full" backgroundColor="bg-white hover:bg-pink-100" border="border border-primary">Deletar Tarefa</Button>
+          <Button
+            style="py-3 w-full mb-2 md:py-2 lg:text-sm"
+          >Editar</Button>
+          <Button
+            style="py-3 w-full md:py-2 lg:text-sm"
+            backgroundColor="bg-white hover:bg-pink-100 "
+            border="border border-primary"
+          >Deletar Tarefa</Button>
         </div>
       </article>
     </>
--- src/pages/components/MainContent/TaskCardView.tsx
+++ src/pages/components/MainContent/TaskCardView.tsx
@@ -5,7 +5,7 @@ import { Task } from "../../../interfaces/task";
 export function TaskCardView(props: PropsWithChildren<{ tasks: Array<Task> }>) {
   return (
     <>
-      <section className="flex flex-col gap-5 mt-6 items-center md:flex-row flex-wrap">
+      <section className="flex flex-col gap-2 mt-6 items-center md:flex-row flex-wrap">
         {props.tasks.map((task, key) => {
           return (
             <TaskCard key={key} task={task} />
