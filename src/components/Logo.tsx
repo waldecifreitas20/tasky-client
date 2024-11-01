ab13904b4bdaf096030e0137745695949c1e8ce6
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-11-01 15:05:45 -0300

    refactor: resizeable logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,12 +1,16 @@
-export function Logo(props: { isResponsible?: boolean }) {
+interface LogoProps {
+  isResponsible?: boolean,
+  textSize?: "text-2xl" | "text-4xl",
+}
+
+export function Logo(props: LogoProps) {
   return (
     <>
       <div className="flex justify-start items-center">
-        <img className="h-[50px]" src="/task-icon.svg" alt="task icone" />
+        <img className={"h-[50px]"} src="/task-icon.svg" alt="task icone" />
         <h1 className={`
-          text-accent 
-          text-2xl 
-          ml-2 
+          text-accent ml-2
+          ${props.textSize ?? "text-2xl"} 
           ${props.isResponsible ? "hidden md:block" : ""}
         `}>Tasky</h1>
       </div>
