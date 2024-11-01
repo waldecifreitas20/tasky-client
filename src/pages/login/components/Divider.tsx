ec53a5ff54beed0df8edc92f808c66b232565d93
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-11-01 15:54:19 -0300

    feat: login page full responsible

--- src/pages/login/components/Divider.tsx
+++ src/pages/login/components/Divider.tsx
@@ -1,7 +1,7 @@
-export function Divider() {
+export function Divider(props: { margins: string }) {
   return (
     <>
-      <div className="flex my-10 w-full">
+      <div className={`flex ${props.margins ?? "my-10"} w-full`}>
         <span className="block h-[1px] bg-white opacity-70 w-full min-w-[100px]"></span>
         <span className="relative -top-2 mx-2 text-xs">ou</span>
         <span className="block h-[1px] bg-white opacity-70 w-full min-w-[100px]"></span>
