9bbd54d1a50b12e39a2e8832350c6cdf0236ccb7
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-11-03 15:06:10 -0300

    refactor: Tasky API requester module

---
+++ src/services/taskyApi.ts
@@ -0,0 +1,57 @@
+export interface TaskyApiGetRequest {
+  route: string,
+  authorization?: string,
+}
+export interface TaskyApiPostRequest extends TaskyApiGetRequest {
+  body: any;
+}
+
+async function GET(params: TaskyApiGetRequest) {
+  return await fetch(`http://localhost:3000/${params.route}`, {
+    method: "GET",
+    headers: {
+      'Content-Type': 'application/json',
+      'authorization': params.authorization ?? ""
+    },
+  }).then(async response => {
+    return {
+      status: response.status,
+      body: await response.json()
+    };
+  }).catch((err) => {
+    console.log(err);
+    return {
+      status: 502,
+      body: "Deu merda"
+    };
+  });
+}
+
+async function POST(params: TaskyApiPostRequest) {
+  return await fetch(`http://localhost:3000/${params.route}`, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      'authorization': params.authorization ?? ""
+    },
+    body: JSON.stringify(params.body)
+  }).then(async response => {
+    console.log(response);
+    
+    return {
+      status: response.status,
+      body: await response.json()
+    };
+  }).catch((err) => {
+    console.log(err);
+    return {
+      status: 502,
+      body: "Deu merda"
+    };
+  });
+}
+
+export const TaskyApi = {
+  GET,
+  POST,
+}
