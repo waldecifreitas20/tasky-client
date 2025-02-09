ad0fed8591daf53156f6fef43eabe851eb16a6db
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2025-02-09 13:06:33 -0300

    fix: return type on openeditable function

--- src/contexts/TaskFormContext.tsx
+++ src/contexts/TaskFormContext.tsx
@@ -3,7 +3,7 @@ import { Task } from "../interfaces/task";
 
 export interface TaskFormActions {
   open: CallableFunction
-  openEditable: (data: Task) => {}
+  openEditable: (data: Task) => void; 
 };
 
 export const TaskFormContext = createContext<TaskFormActions | undefined>(undefined);
